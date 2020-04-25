6af42c950a1f3d507d8ed9ebcda07b65c8af5d04
erikfuego <tuf98039@temple.edu>
2020-04-25 13:47:54 -0400

    added spin method for player class

--- Player.py
+++ Player.py
@@ -5,11 +5,16 @@ Created on 4/24/2020
 
 Description of file: 
 """
+from random import randint
+
 
 class Player(object):
 
-    def __init__(self, name, turn, position=0):
+    def __init__(self, name, turn=0, position=0):
 
         self.name = name
         self.turn = turn
         self.position = position
+
+    def spin(self):
+        return randint(1, 6)
