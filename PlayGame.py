10843029f25f25b5efe3a3887103c9a8b3920e80
erikfuego <tuf98039@temple.edu>
2020-04-25 02:11:21 -0400

    added code to handle moving over 100 or to win the game

--- PlayGame.py
+++ PlayGame.py
@@ -25,9 +25,17 @@ class PlayGame(object):
                 value = input("\nPlease type in \"SPIN\" and press ENTER: ")
                 if value == 'SPIN':
                     spin = Spin()
-                    player.position += int(spin)
-                    print(f"\nYou spun a {spin}! You moved up {spin} spaces to {player.position}.")
-                    board = BOARD[player.position]
-                    event = EVENTS[board[1]]
-                    player.position = board[0]
-                    print(event % player.position)
+                    if not (player.position + int(spin)) > 100:
+                        player.position += int(spin)
+                        print(f"\nYou spun a {spin}! You moved up {spin} spaces to {player.position}.")
+                        board = BOARD[player.position]
+                        event = EVENTS[board[1]]
+                        player.position = board[0]
+                        print(event % player.position)
+                        if player.position == 100:
+                            print(f"\n{(player.name).upper()} HAS WON THE GAME!!!")
+                            print("\nThanks for playing!")
+                            exit()
+                    else:
+                        print(f"\nYou spun a {spin}!")
+                        print(f"You need a {100-player.position} to win the game or lower to move spaces.")
