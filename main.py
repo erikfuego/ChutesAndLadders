9137e8efe17de67028e6868c7aa2014fb3dc0897
erikfuego <tuf98039@temple.edu>
2020-04-25 13:48:22 -0400

    updated main to use functions

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ Created on 4/24/2020
 
 Description of file:
 """
-from PlayGame import PlayGame
-from Spin import Spin
-from Player import Player
+from create_player import create_player
+from play_game import PlayGame
+from sort_players import sorted_players, sort_players
 
 print(
 """
@@ -18,49 +18,51 @@ players = input("How many people will be playing (1-4): ")
 
 print("\nOkay! %s players will be playing." % players)
 
-PLAYERS = dict()
-
-FIRST_ROLLS = []
+# total_players = dict()
+#
+# first_spins = []
 
 for i in range(1, int(players)+1):
+    player = create_player(i)
 
-    player_name = input("\nPlayer %s, what is your name: " % i)
-    print("\n%s, please spin to determine the order of players." % player_name)
-    spin = input("\nType in \"SPIN\" and press ENTER: ")
+sort_players()
+# print("Hello World")
+#
+#     player_name = input("\nPlayer %s, what is your name: " % i)
+#     print("\n%s, please spin to determine the order of players." % player_name)
+#     spin = input("\nType in \"SPIN\" and press ENTER: ")
+#
+#     if spin == 'SPIN':
+#         turn = Spin()
+#         print("\n%s, you spun a %s." % (player_name, turn))
+#
+#         if turn not in total_players.keys():
+#             total_players[turn] = Player(name=player_name, turn=turn)
+#             first_spins.append(int(turn))
+#         else:
+#             temp = turn
+#             while temp in total_players.keys():
+#                 turn = int(turn) - 1
+#                 if turn not in total_players.keys():
+#                     total_players[turn] = Player(name=player_name, turn=str(turn))
+#                     first_spins.append(int(turn))
+#                     break
 
-    if spin == 'SPIN':
-        turn = Spin()
-        print("\n%s, you spun a %s." % (player_name, turn))
+# for i in sorted(first_spins):
+#     SORTED_PLAYERS.insert(0, total_players[i])
+#
+# VALUES = {
+#     1: 'First',
+#     2: 'Second',
+#     3: 'Third',
+#     4: 'Fourth'
+# }
+#
+# for index, player in enumerate(SORTED_PLAYERS):
+#     player.turn = VALUES[index+1]
+#     print(f"\n{player.name} is {VALUES[index+1]}!")
 
-        if turn not in PLAYERS.keys():
-            PLAYERS[turn] = Player(name=player_name, turn=turn)
-            FIRST_ROLLS.append(int(turn))
-        else:
-            temp = turn
-            while temp in PLAYERS.keys():
-                turn = int(turn) - 1
-                if turn not in PLAYERS.keys():
-                    PLAYERS[turn] = Player(name=player_name, turn=str(turn))
-                    FIRST_ROLLS.append(int(turn))
-                    break
-
-SORTED_PLAYERS = []
-
-for i in sorted(FIRST_ROLLS):
-    SORTED_PLAYERS.insert(0, PLAYERS[i])
-
-VALUES = {
-    1: 'First',
-    2: 'Second',
-    3: 'Third',
-    4: 'Fourth'
-}
-
-for index, player in enumerate(SORTED_PLAYERS):
-    player.turn = VALUES[index+1]
-    print(f"\n{player.name} is {VALUES[index+1]}!")
-
-game = PlayGame(players=SORTED_PLAYERS)
+game = PlayGame(players=sorted_players)
 
 game.run()
 
