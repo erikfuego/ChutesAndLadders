07530597fb0c1f3c1af900155abafa5c651c23bc
erikfuego <tuf98039@temple.edu>
2020-04-24 23:00:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Created on 4/24/2020
+
+@author: erosales
+
+Description of file: 
+"""
+print(
+"""
+Welcome to Chutes And Ladders!
+""")
+
+players = raw_inpurt("How many people will be playing (1-4)?")
